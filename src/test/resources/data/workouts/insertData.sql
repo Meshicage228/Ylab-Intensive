38a9e835b3dede8bfd1477469cc13bb16ff1a57e
Vlad.Zherko <vladikon19@tut.by>
2024-05-17 01:03:54 +0300

    init : sql for tests

---
+++ src/test/resources/data/workouts/insertData.sql
@@ -0,0 +1,4 @@
+INSERT INTO entities.workouts VALUES
+                                  (1, 1, 1, 120, 120, 'After each dinner', '2024-04-18', '2024-04-18'),
+                                  (2, 1, 2, 240, 20, 'Every morning', '2024-04-18', '2024-04-18'),
+                                  (3, 2, 3, 10, 30, 'Helps mental health', '2024-04-16', '2024-04-16');
